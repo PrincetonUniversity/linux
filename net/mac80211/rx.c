a17f5db763a33fe69d8ed3b182840581e9f4d59d
Hansen Qian <hansenq@gmail.com>
2015-11-28 19:41:23 -0800

    More Print statements

--- net/mac80211/rx.c
+++ net/mac80211/rx.c
@@ -3547,6 +3547,8 @@ static void __ieee80211_rx_handle_packet(struct ieee80211_hw *hw,
 void ieee80211_rx_napi(struct ieee80211_hw *hw, struct sk_buff *skb,
 		       struct napi_struct *napi)
 {
+	printk(KERN_DEBUG "MC - rx.c: ieee80211_rx_napi\n");
+	printk(KERN_INFO "MC - rx.c: ieee80211_rx_napi\n");
 	struct ieee80211_local *local = hw_to_local(hw);
 	struct ieee80211_rate *rate = NULL;
 	struct ieee80211_supported_band *sband;
@@ -3658,6 +3660,8 @@ EXPORT_SYMBOL(ieee80211_rx_napi);
  * context. Post the skb on the queue and schedule the tasklet */
 void ieee80211_rx_irqsafe(struct ieee80211_hw *hw, struct sk_buff *skb)
 {
+	printk(KERN_DEBUG "MC - rx.c: ieee80211_rx_irqsafe\n");
+	printk(KERN_INFO "MC - rx.c: ieee80211_rx_irqsafe\n");
 	struct ieee80211_local *local = hw_to_local(hw);
 
 	BUILD_BUG_ON(sizeof(struct ieee80211_rx_status) > sizeof(skb->cb));
